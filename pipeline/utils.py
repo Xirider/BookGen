af4c82c9d2eef24b05a078b2b9210f145036e689
Xirid <peter.k.albert@gmail.com>
2020-01-08 20:12:25 +0100

    tested everything

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -2,22 +2,50 @@ import os
 import pickle
 import json
 from transformers import GPT2Tokenizer
+import random
 
 def get_tokenizer(tokenizer_name = "gpt2-large"):
     tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_name)
 
     special_tokens_dict = {"pad_token": "<pad>", "additional_special_tokens":["<genstart>", "<prevtext>",
-     "<sum1>", "<sum2>", "<sum3>", "<sum4>", "<sum5>", "<sum6>", "<sum7>", "<sum8>",
-      "<sum9>", "<sum10>", "<sum_chapters>", "<chapter>", "<sum_end>"]}
+    "<sum1>", "<sum2>", "<sum3>", "<sum4>", "<sum5>", "<sum6>", "<sum7>", "<sum8>",
+    "<sum9>", "<sum10>", "<sum_chapters>", "<chapter>", "<sum_end>"]}
     num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
 
     return tokenizer
 
 
 def save_datasets(datasets, name="datasets"):
-  name = "pipeline/data/" + name + ".json"
-  with open(name, "w") as fp:
-    json.dump(datasets, fp)
+    name = "pipeline/data/" + name + ".json"
+    with open(name, "w") as fp:
+        json.dump(datasets, fp)
+
+
+
+def split_examples(datasets, ratio):
+    ratio = 1 - ratio
+    train_datasets = {}
+    eval_datasets = {}
+    for key in datasets:
+        train_datasets[key] = []
+        eval_datasets[key] = []
+
+    for key in datasets:
+        random.shuffle(datasets[key])
+        item_number = len(datasets[key])
+        
+        cutoff = int(item_number * ratio)
+        if item_number > 0:
+            if cutoff == 0:
+                cutoff += 1
+            
+            train_datasets[key] = datasets[key][:cutoff]
+
+            eval_datasets[key] = datasets[key][cutoff:]
+
+    datasets = []
+    return train_datasets, eval_datasets
+            
 
 
 # def cache_object(obj, cache_name, no_cache_flag=False, reset_cache=False):
--- piperunner.py
+++ piperunner.py
@@ -3,7 +3,7 @@ from pipeline.filterstories import filter_ff_stories
 from pipeline.split import split
 from pipeline.summarize import summarize_books
 from pipeline.lmprep import prepare_for_lm
-from pipeline.utils import get_tokenizer, save_datasets
+from pipeline.utils import get_tokenizer, save_datasets, split_examples
 
 
 def run():
@@ -14,7 +14,9 @@ def run():
     books = split(books, tokenizer, max_tokens = 200, max_prev_tokens = 100)
     books = summarize_books(books, max_chapter_level=10, sum_ratio=3, sum_model="bart")
     datasets = prepare_for_lm(books, tokenizer, max_seq_len=400)
-    save_datasets(datasets)
+    train_datasets, eval_datasets = split_examples(datasets, ratio=0.1)
+    save_datasets(train_datasets, "train")
+    save_datasets(eval_datasets, "eval")
 
     
     return datasets
